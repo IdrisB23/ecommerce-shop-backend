a59364d537db68b870217315bb55e65e00a2a90d
Idris Babay <idrissbabay@gmail.com>
2024-12-05 11:27:53 +0100

    Create model classes for orders and users

---
+++ src/main/java/com/example/dailycodework/dream_shops/model/Order.java
@@ -0,0 +1,34 @@
+package com.example.dailycodework.dream_shops.model;
+
+import com.example.dailycodework.dream_shops.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter @Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+
+    // how the attribute (column) will be stored in the DB. ENUM vals are stored as String
+    // counter-part is ORDINAL (1,2,3,etc.)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
