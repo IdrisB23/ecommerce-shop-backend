f459d3641a9c8f3aba38aa2df30ad9d85ea58f8d
Idris Babay <idrissbabay@gmail.com>
2024-12-08 20:41:42 +0100

    Add Spring Service to initialize 5 users upon Boot for ease of API-testing

---
+++ src/main/java/com/example/dailycodework/dream_shops/data/DataInitializer.java
@@ -0,0 +1,37 @@
+package com.example.dailycodework.dream_shops.data;
+
+import com.example.dailycodework.dream_shops.model.User;
+import com.example.dailycodework.dream_shops.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+    private final UserRepository userRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        createDefaultUserIfNotExits();
+    }
+
+
+    private void createDefaultUserIfNotExits(){
+        for (int i = 1; i<=5; i++){
+            String defaultEmail = "user"+i+"@email.com";
+            if (userRepository.existsByEmail(defaultEmail)){
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("The User");
+            user.setLastName("User" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword("123456");
+            userRepository.save(user);
+            System.out.println("Default vet user " + i + " created successfully.");
+        }
+    }
+}
