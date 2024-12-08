1d537d54c18a109f7fc08007fe28459239fa6c5b
Idris Babay <idrissbabay@gmail.com>
2024-12-08 16:07:14 +0100

    Add @Table annotation to avoid JPA issue not creating requested table due to conflicts (naming), add @JSONIgnore

--- src/main/java/com/example/dailycodework/dream_shops/model/Cart.java
+++ src/main/java/com/example/dailycodework/dream_shops/model/Cart.java
@@ -25,7 +25,7 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> cartItems = new HashSet<>();
 
-    @OneToOne
+    @JsonIgnore @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
 
--- src/main/java/com/example/dailycodework/dream_shops/model/Order.java
+++ src/main/java/com/example/dailycodework/dream_shops/model/Order.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @Getter @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "orders")
 public class Order {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
