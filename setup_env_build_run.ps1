b7514b6a03f3bf76d451aec339f4350fc046d1f8
Idris Babay <idrissbabay@gmail.com>
2024-12-29 14:11:20 +0100

    Update powershell script file with leading .\ to execute the maven wrapper

--- setup_env_build_run.ps1
+++ setup_env_build_run.ps1
@@ -5,13 +5,13 @@ if (Test-Path $buildPath) {
     # If the directory exists, do something here
     Write-Host "Build Directory exists"
     Write-Host "Cleaning Maven Build"
-    mvnw clean
+    .\mvnw clean
 } else {
     # If the directory does not exist, do something else here
     Write-Host "Build Directory does not exist"
 }
 
 Write-Host "Packaging Maven Project as JAR"
-mvnw package
+.\mvnw package
 Write-Host "Launching JAR"
 java -jar .\target\dream-shops-0.0.1-SNAPSHOT.jar
