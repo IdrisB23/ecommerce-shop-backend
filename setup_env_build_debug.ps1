3875fa0cd5125f4dc0dd7c435eecbfc53a552278
Idris Babay <idrissbabay@gmail.com>
2025-01-02 22:38:24 +0100

    fix executing mvnw from Powershell script

--- setup_env_build_debug.ps1
+++ setup_env_build_debug.ps1
@@ -5,14 +5,14 @@ if (Test-Path $buildPath) {
     # If the directory exists, do something here
     Write-Host "Build Directory exists"
     Write-Host "Cleaning Maven Build"
-    mvnw clean
+    .\mvnw clean
 } else {
     # If the directory does not exist, do something else here
     Write-Host "Build Directory does not exist"
 }
 
 Write-Host "Packaging Maven Project as JAR"
-mvnw package
+.\mvnw package
 Write-Host "Launching JAR in DEBUG MODE"
 
 java -agentlib:jdwp=transport=dt_shmem,server=y,suspend=n,address=1337 -jar .\target\dream-shops-0.0.1-SNAPSHOT.jar
